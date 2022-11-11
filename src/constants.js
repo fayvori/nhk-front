28342a68b621b168ae266ae032eb6e5fb45ea57e
Ignat Belousov <ignat.belousov2000@yahoo.com>
2022-11-11 05:56:47 +0300

    change debug to false

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const DEBUG = true
+const DEBUG = false
 export const API_URL = DEBUG ? "http://localhost:5119" : "http://51.250.104.108:5119"
 export const RENT_MEDIA_URL = "https://media.rentinhand.ru"
 export const HASHER_KEY = "K4uspvbHxw&qChFZa+BRLUAq+ZJyyF^mpsEtmxNP&5n(ZFeA+%mRL3P#nG!VbLcM"
